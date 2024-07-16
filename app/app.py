1fbc8df9f71074c717a2e1a1b08051fa6e4fd7e4
Gauaravgupta <ggupta160493@gmail.com>
2024-07-16 19:51:38 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -62,7 +62,7 @@ def get_sentiment_analyzed_comments():
     return jsonify({'comments': analyzed_comments})
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host='0.0.0.0',port=5000,debug=False)
 
 
 # In[ ]:
